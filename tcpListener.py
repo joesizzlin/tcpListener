390c132ad49cc745e159d20d54d30c69b404e7b1
joesizzlin <cassellajoe@gmail.com>
2023-01-07 17:39:28 -0500

    initial commit

---
+++ tcpListener.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import socket
+
+TCP_IP = "192.168.0.122"
+TCP_PORT = 6996
+BUFFER_SIZE = 100
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connection address: ', addr)
+
